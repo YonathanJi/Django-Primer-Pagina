61042c2395fd60bfb85ba6c3c5d3455572c19e41
YonathanJi <yonathan.jimenez00@usc.edu.co>
2023-12-06 15:18:43 -0500

    bdposgreslq

--- Web1/settings.py
+++ Web1/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'Web1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'articulosclientes',
+        'USER':'postgres',
+        'PASSWORD':'Jonathan5896',
+        'HOST':'localhost',
+        'DATABASE_PORT':'5432',
+
     }
 }
 
